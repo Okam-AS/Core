05848c5e10c52185bb21e80d74ebf2950820eb39
asharghi <a.sharghi.mail@gmail.com>
2021-12-21 13:56:59 +0100

    Added tools

---
+++ helpers/tools.ts
@@ -0,0 +1,33 @@
+const wholeAmountTool = (amount: Number): string => {
+  if (!amount) { return '0' }
+  const wholeAmount = amount.toString().slice(0, -2)
+  return wholeAmount ? wholeAmount.replace(/\B(?=(\d{3})+(?!\d))/g, ' ') : '0'
+}
+
+const fractionAmountTool = (amount: Number): string => {
+  if (!amount) { return '00' }
+  const fractionAmount = amount.toString().slice(-2)
+  return fractionAmount.length < 2 ? '00' : fractionAmount
+}
+
+const priceLabelTool = (totalPrice: Number, hideFractionIfZero: Boolean) => {
+  const prefix = 'kr '
+  const wholeAmount = wholeAmountTool(totalPrice)
+  let fraction = ''
+  if (!hideFractionIfZero || parseInt(fractionAmountTool(totalPrice)) > 0) {
+    fraction = ',' + fractionAmountTool(totalPrice)
+  }
+  return prefix + wholeAmount + fraction
+}
+
+const formatStringTool = (str: String, format: Object) => {
+  Object.keys(format).forEach((key) => {
+    str = str.replace(new RegExp('\\{' + key + '\\}', 'gi'), format[key])
+  })
+  return str
+}
+
+export const wholeAmount = wholeAmountTool
+export const fractionAmount = fractionAmountTool
+export const priceLabel = priceLabelTool
+export const formatString = formatStringTool
