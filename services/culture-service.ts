b1897a3adb6e5100ea06cf5d6bd8e532ababf0a0
asharghi <a.sharghi.mail@gmail.com>
2022-01-09 03:32:27 +0100

    Culture service

--- services/culture-service.ts
+++ services/culture-service.ts
@@ -21,18 +21,23 @@ export class CultureService {
     }
 
     public async CreateOrUpdateTranslations (cultures: Array<Culture>): Promise<Product> {
-      const response = await this._requestService.PostRequest('/culture/translations', cultures)
+      const response = await this._requestService.PostRequest('/culture/translation', cultures)
       return this.ParsedResponse(response, 'Kunne ikke lagre språk')
     }
 
     public async Create (code: string): Promise<Product> {
-      const response = await this._requestService.PostRequest('/culture/translations/' + code)
+      const response = await this._requestService.PostRequest('/culture/' + code)
       return this.ParsedResponse(response, 'Kunne ikke opprette språk')
     }
 
-    public async Delete (translationKey: string): Promise<void> {
-      const response = await this._requestService.DeleteRequest('/culture/' + translationKey)
+    public async DeleteTranslation (translationKey: string): Promise<void> {
+      const response = await this._requestService.DeleteRequest('/culture/translation/' + translationKey)
       if (response.statusCode >= 300 || response.statusCode < 200) { throw new Error('Kunne ikke slette oversettelse') }
+    }
+
+    public async Delete (code: string): Promise<void> {
+      const response = await this._requestService.DeleteRequest('/culture/' + code)
+      if (response.statusCode >= 300 || response.statusCode < 200) { throw new Error('Kunne ikke slette språk') }
     }
 
     private ParsedResponse (response, errorMessage) {
