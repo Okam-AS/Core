f7da14987501ebe2a015362bf196fec9d359a816
asharghi <a.sharghi.mail@gmail.com>
2022-04-11 22:59:29 +0200

    PaymentTypes in Statistic

---
+++ enums/order-payment-type-filter.ts
@@ -0,0 +1,5 @@
+export enum OrderPaymentTypeFilter {
+    PayInStore = 'PayInStore',
+    Stripe = 'Stripe',
+    Vipps = 'Vipps'
+}
--- models/statistic/statistic-query-orders.ts
+++ models/statistic/statistic-query-orders.ts
@@ -1,10 +1,11 @@
-import { OrderStatusFilter, DeliveryType } from '../../enums'
+import { OrderStatusFilter, DeliveryType, OrderPaymentTypeFilter } from '../../enums'
 
 export class StatisticQueryOrders {
     storeId: number;
     from: string;
     to: string;
     statuses: Array<OrderStatusFilter>;
+    paymentTypes: Array<OrderPaymentTypeFilter>;
     deliveryTypes: Array<DeliveryType>;
     includeItems: boolean;
 }
