e04f47b1c1828a411969a80ba49b48b3edd5531a
asharghi <a.sharghi.mail@gmail.com>
2023-03-06 00:10:53 +0100

    Add theme colors

--- v4/pinia/colors.ts
+++ v4/pinia/colors.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useSettings } from "./";
+import { defaultTheme } from "../themes";
 
 export const useColors = defineStore("colors", () => {
   const colors = ref({
@@ -10,12 +11,32 @@ export const useColors = defineStore("colors", () => {
   });
 
   const $color = (key: string, darkmodeKey?: string) => {
-    return darkmodeKey && useSettings().$isDarkmode && colors.value.hasOwnProperty(darkmodeKey)? 
+    return darkmodeKey && useSettings().$isDarkmode && colors.value.hasOwnProperty(darkmodeKey) ?
       colors.value[darkmodeKey] :
       colors.value[key]
   }
 
+  const $primaryColor = computed(() => {
+    return $color(defaultTheme.light.primaryColor, defaultTheme.dark.primaryColor)
+  })
+
+  const $secondaryColor = computed(() => {
+    return $color(defaultTheme.light.secondaryColor, defaultTheme.dark.secondaryColor)
+  })
+
+  const $backgroundColor = computed(() => {
+    return $color(defaultTheme.light.backgroundColor, defaultTheme.dark.backgroundColor)
+  })
+
+  const $textColor = computed(() => {
+    return $color(defaultTheme.light.textColor, defaultTheme.dark.textColor)
+  })
+
   return {
-    $color
+    $color,
+    $primaryColor,
+    $secondaryColor,
+    $backgroundColor,
+    $textColor
   }
 });
---
+++ v4/themes/index.ts
@@ -0,0 +1,29 @@
+export const defaultTheme = {
+	light: {
+		primaryColor: 'darkBlue',
+		secondaryColor: 'darkBlue',
+		backgroundColor: 'lightBlue',
+		textColor: 'darkBlue'
+	},
+	dark: {
+		primaryColor: 'darkBlue',
+		secondaryColor: 'darkBlue',
+		backgroundColor: 'lightBlue',
+		textColor: 'darkBlue'
+	}	
+}
+
+export const ikigaiTheme = {
+	light: {
+		primaryColor: 'darkBlue',
+		secondaryColor: 'darkBlue',
+		backgroundColor: 'lightBlue',
+		textColor: 'darkBlue'
+	},
+	dark: {
+		primaryColor: 'darkBlue',
+		secondaryColor: 'darkBlue',
+		backgroundColor: 'lightBlue',
+		textColor: 'darkBlue'
+	}	
+}
