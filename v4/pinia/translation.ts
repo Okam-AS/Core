e95a582e0d5893db24b9e42e28d37de2d0c7c48d
asharghi <a.sharghi.mail@gmail.com>
2023-06-21 00:22:58 +0200

    Translation using cultureCode

--- v4/pinia/translation.ts
+++ v4/pinia/translation.ts
@@ -1,23 +1,21 @@
 import en from '../translations/en'
 import no from '../translations/no'
 import { defineStore } from "pinia";
+import { useSettings } from "./settings"
+
 import { ref, computed } from "vue";
 
 export const useTranslation = defineStore("translation", () => {
 
-  const selectedLanguage = ref('no');
+  const { $cultureCode } = useSettings()
   const translations = ref({ en, no });
-
-  const setLanguage = (lang: string) => {
-    selectedLanguage.value = lang
-  }
 
   const updateTranslations = (key: string, value: object) => {
     translations.value[key] = value
   }
 
   const $i = computed(() => {
-    if (selectedLanguage.value === 'no') {
+    if ($cultureCode === 'no') {
       return translations.value.no
     } else {
       return translations.value.en
@@ -26,7 +24,6 @@ export const useTranslation = defineStore("translation", () => {
 
   return {
     $i,
-    setLanguage,
     updateTranslations,
   }
 });
--- v4/services/request-service.ts
+++ v4/services/request-service.ts
@@ -64,6 +64,7 @@ export class RequestService {
     }
 
     private DefaultRequest (path: string, payload: any, method: HttpMethod): any {
+      console.log(method + ": " + path)
       return this.BuildRequest(path, method, payload ? JSON.stringify(payload) : '', this._coreInitializer.bearerToken)
     };
 
