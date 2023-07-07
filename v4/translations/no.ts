e3c6a096c67be0381ab40846fc6b6cbd0f51e3d6
asharghi <a.sharghi.mail@gmail.com>
2023-07-08 00:55:37 +0200

    Lang

--- v4/translations/no.ts
+++ v4/translations/no.ts
@@ -5,7 +5,7 @@ export default {
   storePage_orderHere: 'Bestill her',
   storePage_findUsHere: 'Finn oss her',
   productPage_addToCart: "Legg i kurv",
-  productPage_saveToCart: "Lagre endringer",
+  productPage_saveToCart: "Lagre",
   checkoutPage_heading: "Din bestilling",
   checkoutPage_cartIsEmpty: "Du har ingen varer i handlekurven",
   checkoutPage_submit: "Fullør bestilling",
