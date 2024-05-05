b1cc48a1333cacb9dc6a13cc0e057ea7cd844bd9
asharghi <a.sharghi.mail@gmail.com>
2024-05-05 14:15:15 +0200

    Add cutOffDaysForRewardCalculation

--- v4/models/reward/reward-program.ts
+++ v4/models/reward/reward-program.ts
@@ -4,6 +4,7 @@ export class RewardProgram {
   rewardProgramId: string;
   name: string;
   cashbackEnabled: boolean;
+  cutOffDaysForRewardCalculation: number;
   cashbackRanges: Array<RewardCachbackRange>;
   stores: Array<Store>;
   memberships: Array<RewardMembership>;
