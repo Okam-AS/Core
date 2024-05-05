af2b4059bf50d4e7585cab460407cea949f2c782
asharghi <a.sharghi.mail@gmail.com>
2024-05-05 22:30:45 +0200

    Remove stores from RewardProgram

--- models/reward/reward-program.ts
+++ models/reward/reward-program.ts
@@ -1,10 +1,9 @@
-import { Store, RewardMembership, RewardCachbackRange } from '..';
+import { RewardMembership, RewardCachbackRange } from '..';
 
 export class RewardProgram {
     rewardProgramId: string;
     name: string;
     cashbackEnabled: boolean;
     cashbackRanges: Array<RewardCachbackRange>;
-    stores: Array<Store>;
     memberships: Array<RewardMembership>;
 }
