2b7ff32e42d674175707c14ddafb3a0c2c4cabf7
asharghi <a.sharghi.mail@gmail.com>
2024-06-22 00:29:08 +0200

    Add GetMembers

--- services/reward-service.ts
+++ services/reward-service.ts
@@ -12,6 +12,14 @@ export class RewardService {
     this._vuexModule = vuexModule
   }
 
+  public async GetMembers(storeId: number) {
+    const response = await this._requestService.GetRequest('/rewards/members/' + storeId)
+    const parsedResponse = this._requestService.TryParseResponse(response)
+    if (parsedResponse === undefined) { throw new Error('Failed to get members') }
+
+    return parsedResponse
+  }
+
   public async Get(storeId: number) {
     const response = await this._requestService.GetRequest('/rewards/' + storeId)
     const parsedResponse = this._requestService.TryParseResponse(response)
