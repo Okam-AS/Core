718d396aa8cf499d8cbbf86605bcd0a9d74f4bfd
asharghi <a.sharghi.mail@gmail.com>
2024-08-24 15:52:19 +0200

    Add deliveryAdditionalAmount

--- models/product/bulk-import-row.ts
+++ models/product/bulk-import-row.ts
@@ -1,12 +1,13 @@
 export class BulkImportRow {
-    categoryName: string;
-    name: string;
-    otherInformation: string;
-    description: string;
-    priceAmount: number;
-    tax: number;
-    tableAdditionalAmount: number;
-    tableTax: number;
-    depositAmount: number;
-    soldOut: boolean;
+  categoryName: string;
+  name: string;
+  otherInformation: string;
+  description: string;
+  priceAmount: number;
+  tax: number;
+  tableAdditionalAmount: number;
+  deliveryAdditionalAmount: number;
+  tableTax: number;
+  depositAmount: number;
+  soldOut: boolean;
 }
